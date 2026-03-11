211c91b7db5177c1320dfce724689293a679068e
Chukwu Felix <goduknows@gmail.com>
2026-03-11 11:16:59 +0100

    feat: add experimental server components external packages to next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
       transpilePackages: ["next-mdx-remote"],
+      experimental: {
+            serverComponentsExternalPackages: ["rehype-pretty-code", "shiki"],
+      },
 };
 
 export default nextConfig;
